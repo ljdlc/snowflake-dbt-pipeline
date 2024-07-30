b72e02a4a8ecbbae5866d7723d46710c620693d1
ljdlc <lemoel.delacruz@gmail.com>
2024-07-30 16:39:04 -0400

    Add more EDA queries and and table comments

--- data-transformation/project_eda.sql
+++ data-transformation/project_eda.sql
@@ -92,3 +92,53 @@ from
 inner join customer as dim
     on dim.c_customer_sk = fact.cs_bill_customer_sk
 limit 5;
+
+-- How often is the inventory table updated?
+select
+    inv_date_sk,
+    inv_item_sk,
+    count(*) as count
+from inventory
+group by 1,2
+order by 2,1
+limit 20;
+-- inv_date_sk increases increases by 7 for the similar inv_item_sk entries so that must mean inventory is updated every 7 days.
+
+-- Double-check the above assumption. For inv_item_sk = 1, how frequently is it being recorded in the inventory table?
+select distinct
+    date.cal_dt,
+    date.wk_num
+from date_dim date
+inner join inventory inv
+    on date.d_date_sk = inv.inv_date_sk
+    and inv_item_sk = 1
+order by 1,2;
+-- Assumption is correct, for inv_item_sk = 1, new entries are added every 7 days/every week
+
+-- For inv_item_sk = 1, how frequently is it being sold by web sales?
+select distinct
+    date.cal_dt,
+    date.wk_num
+from web_sales ws
+inner join date_dim date
+    on date.d_date_sk = ws.ws_sold_date_sk
+where ws.ws_item_sk=1
+order by 1;
+--  There is no specific frequency or trend of sales for item inv_item_sk = 1
+
+-- How many total items are there?
+select
+    count(distinct i_item_sk) as total_items
+from item;
+-- 18000 items 
+
+-- How many individual customers?
+select
+    count(distinct c_customer_sk) as total_customers
+from customer;
+-- 100,000 customers
+
+-- Add table descriptions
+comment on table date_dim is 'This table is used to convert d_date_sk key in other tables to calendar date. This is the date dimension in the source.';
+
+comment on column date_dim.cal_dt is 'Calendar Date';
