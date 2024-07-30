c10e3757b11980be860ac6694dafcc65e5d5e05e
ljdlc <lemoel.delacruz@gmail.com>
2024-07-29 22:22:33 -0400

    Check surrogate key connections in EDA script

--- data-transformation/project_eda.sql
+++ data-transformation/project_eda.sql
@@ -34,3 +34,61 @@ from date_dim
 where d_date_sk = (select max(WS_SOLD_DATE_SK) from web_sales);
 -- 2024-05-29
 
+
+-- Genearlly, how many catalog sales per day?
+select
+    d.cal_dt,
+    cs.cs_sold_date_sk,
+    count(*)
+from
+    catalog_sales cs
+inner join date_dim d
+    on cs.cs_sold_date_sk=d.d_date_sk
+group by 1,2
+order by 1 desc
+limit 10;
+-- around 400-500 sales per day
+
+-- Genearlly, how many web sales per day?
+select
+    d.cal_dt,
+    ws.ws_sold_date_sk,
+    count(*)
+from
+    web_sales ws
+inner join date_dim d
+    on ws.ws_sold_date_sk=d.d_date_sk
+group by 1,2
+order by 1 desc
+limit 10;
+-- around 200-400 sales per day
+
+-- Verify date_dim and cs_sold_date_sk have corresponding values
+select
+    fact.cs_sold_date_sk,
+    dim.*
+from catalog_sales fact
+inner join date_dim dim
+    on dim.d_date_sk = fact.cs_sold_date_sk
+limit 5;
+-- cs_sold_date_sk correctly has matching values in date dimension
+
+-- Verify time_dim and cs_sold_time_sk have corresponding values
+select
+    fact.cs_sold_date_sk,
+    dim.*
+from catalog_sales fact
+inner join time_dim dim
+    on dim.t_time_sk = fact.cs_sold_time_sk
+limit 5;
+-- cs_sold_time_sk correctly has matching values in time dimension
+
+-- Verify connection bewteen catalog sales and customer cs_bill_customer table
+select
+    distinct fact.cs_bill_customer_sk,
+    dim.*
+from
+    catalog_sales as fact
+inner join customer as dim
+    on dim.c_customer_sk = fact.cs_bill_customer_sk
+limit 5;
